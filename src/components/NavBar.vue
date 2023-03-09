ffd07b47ff93f869f4b8416c45a01054aa9a9938
Lebo04 <nobomvukelebogile497@gmail.com>
2023-03-09 12:49:23 +0200

    Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,24 +9,25 @@
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="#"><router-link to="/">Home</router-link></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Action</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
-                      <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+                    <a class="nav-link" href="#"><router-link to="/about">About</router-link></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link" href="#"><router-link to="/login">LogIn</router-link></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link"><router-link to="/signup">SignUp</router-link></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link"><router-link to="/books">Books</router-link></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link"><router-link to="/cart">Cart</router-link></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link"><router-link to="/contact">Contact Us</router-link></a>
                   </li>
                 </ul>
                 <form class="d-flex" role="search">
